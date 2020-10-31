104d98330aa8aade81b78553e0889aa864dc532a
Abhinav Singh <aks.singh774@gmail.com>
2020-10-31 10:23:01 +0530

    change one

--- eye detector.py
+++ eye detector.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 30 19:21:18 2020
 
-@author: hp
-"""
 
 import cv2
 import numpy as np
@@ -12,26 +7,7 @@ from face_landmarks import get_landmark_model, detect_marks
 
 
 def eye_on_mask(mask, side, shape):
-    """
-    Create ROI on mask of the size of eyes and also find the extreme points of each eye
-
-    Parameters
-    ----------
-    mask : np.uint8
-        Blank mask to draw eyes on
-    side : list of int
-        the facial landmark numbers of eyes
-    shape : Array of uint32
-        Facial landmarks
-
-    Returns
-    -------
-    mask : np.uint8
-        Mask with region of interest drawn
-    [l, t, r, b] : list
-        left, top, right, and bottommost points of ROI
-
-    """
+    
     points = [shape[i] for i in side]
     points = np.array(points, dtype=np.int32)
     mask = cv2.fillConvexPoly(mask, points, 255)
@@ -42,7 +18,7 @@ def eye_on_mask(mask, side, shape):
     return mask, [l, t, r, b]
 
 def find_eyeball_position(end_points, cx, cy):
-    """Find and return the eyeball positions, i.e. left or right or top or normal"""
+    
     x_ratio = (end_points[0] - cx)/(cx - end_points[2])
     y_ratio = (cy - end_points[1])/(end_points[3] - cy)
     if x_ratio > 3:
@@ -56,32 +32,7 @@ def find_eyeball_position(end_points, cx, cy):
 
     
 def contouring(thresh, mid, img, end_points, right=False):
-    """
-    Find the largest contour on an image divided by a midpoint and subsequently the eye position
-
-    Parameters
-    ----------
-    thresh : Array of uint8
-        Thresholded image of one side containing the eyeball
-    mid : int
-        The mid point between the eyes
-    img : Array of uint8
-        Original Image
-    end_points : list
-        List containing the exteme points of eye
-    right : boolean, optional
-        Whether calculating for right eye or left eye. The default is False.
-
-    Returns
-    -------
-    pos: int
-        the position where eyeball is:
-            0 for normal
-            1 for left
-            2 for right
-            3 for up
-
-    """
+    
     cnts, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
     try:
         cnt = max(cnts, key = cv2.contourArea)
@@ -97,20 +48,7 @@ def contouring(thresh, mid, img, end_points, right=False):
         pass
     
 def process_thresh(thresh):
-    """
-    Preprocessing the thresholded image
-
-    Parameters
-    ----------
-    thresh : Array of uint8
-        Thresholded image to preprocess
-
-    Returns
-    -------
-    thresh : Array of uint8
-        Processed thresholded image
-
-    """
+    
     thresh = cv2.erode(thresh, None, iterations=2) 
     thresh = cv2.dilate(thresh, None, iterations=4) 
     thresh = cv2.medianBlur(thresh, 3) 
@@ -118,23 +56,7 @@ def process_thresh(thresh):
     return thresh
 
 def print_eye_pos(img, left, right):
-    """
-    Print the side where eye is looking and display on image
-
-    Parameters
-    ----------
-    img : Array of uint8
-        Image to display on
-    left : int
-        Position obtained of left eye.
-    right : int
-        Position obtained of right eye.
-
-    Returns
-    -------
-    None.
-
-    """
+    
     if left == right and left != 0:
         text = ''
         if left == 1:
@@ -189,8 +111,6 @@ while(True):
         eyeball_pos_left = contouring(thresh[:, 0:mid], mid, img, end_points_left)
         eyeball_pos_right = contouring(thresh[:, mid:], mid, img, end_points_right, True)
         print_eye_pos(img, eyeball_pos_left, eyeball_pos_right)
-        # for (x, y) in shape[36:48]:
-        #     cv2.circle(img, (x, y), 2, (255, 0, 0), -1)
         
     cv2.imshow('eyes', img)
     cv2.imshow("image", thresh)
